f87669ac5b2e1d3c6773098b21ec934dcf6fe0d8
Davide Pirolo <dpirolo@protoqsar.com>
2026-05-22 10:27:28 +0200

    removed the flow to use conda and added the flow to use the native python environment instead

--- src/main/java/insilico/vega/gui/resources/VegaVersion.java
+++ src/main/java/insilico/vega/gui/resources/VegaVersion.java
@@ -19,8 +19,8 @@ public class VegaVersion {
     public static boolean USE_PYTHON_MODELS = false;
 
     public final static String AppName = "VEGA in silico platform";
-    public final static String BuildDate = "29/01/2026";
-    public final static String Version = "1.2.6";
+    public final static String BuildDate = "22/05/2026";
+    public final static String Version = "1.2.7b1";
     public final static int VersionMajor = 1;
     public final static int VersionMinor = 2;
     public final static int VersionRevision = 6;
